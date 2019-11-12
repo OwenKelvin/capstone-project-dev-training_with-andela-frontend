de997f861215f74a27c61ffd9727b9ddb4186656
Owen Kelvin <otienoowenkelvin@gmail.com>
2019-11-13 00:05:44 +0300

    Changed Script file for serve
    Changed script to inbuilt server

--- server.js
+++ server.js
@@ -4,12 +4,10 @@ const app = express();
 
 app.use(express.static(path.join(__dirname, "build")));
 
-app.get("/ping", function(req, res) {
-  return res.send("pong");
-});
-
 app.get("/", function(req, res) {
   res.sendFile(path.join(__dirname, "build", "index.html"));
 });
 
-app.listen(process.env.PORT || 4151);
+app.listen(process.env.PORT || 4000 );
+
+console.log('Server Running...');
