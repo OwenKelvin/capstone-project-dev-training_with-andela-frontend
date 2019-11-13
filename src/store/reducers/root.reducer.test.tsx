ef637b3b4a3762a059400d25fda2f97374d83ccd
Owen Kelvin <otienoowenkelvin@gmail.com>
2019-11-13 20:53:04 +0300

    Added test files for store
    - root store
    - login store
    - login reducer
    
    solves #169737073
    see https://www.pivotaltracker.com/story/show/169737073

---
+++ src/store/reducers/root.reducer.test.tsx
@@ -0,0 +1,7 @@
+import rootReducer from './root.reducer';
+
+describe('ROOT REDUCER: ', () => {
+    it('should export loginReducer', () => {
+        expect(rootReducer).toBeDefined();
+    });
+});
