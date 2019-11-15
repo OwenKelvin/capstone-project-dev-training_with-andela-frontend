33b2a52d50d1cad4d308d3b31873767c7944a71f
OwenKelvin <53997274+OwenKelvin@users.noreply.github.com>
2019-11-15 13:30:02 +0300

    Fixed Provider/ App defined but not used error

--- src/index.tsx
+++ src/index.tsx
@@ -1,8 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { Provider } from 'react-redux';
+// import { Provider } from 'react-redux';
 import './index.css';
-import App from './App';
+// import App from './App';
 import * as serviceWorker from './serviceWorker';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import configureStore from './store';
