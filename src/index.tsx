4f48b26ecc45e79104ce9971c1caff0862c29f47
OwenKelvin <53997274+OwenKelvin@users.noreply.github.com>
2019-11-15 13:09:21 +0300

    Update index.tsx
    added Root to the index.tsx file

--- src/index.tsx
+++ src/index.tsx
@@ -7,9 +7,11 @@ import * as serviceWorker from './serviceWorker';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import configureStore from './store';
 
-ReactDOM.render(<Provider store={configureStore()}>
-  <App />
-</Provider>, document.getElementById('root'));
+ReactDOM.render(<Root store={configureStore()} />, 
+  document.getElementById('root'));
+// ReactDOM.render(<Provider store={configureStore()}>
+//  <App />
+// </Provider>, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
