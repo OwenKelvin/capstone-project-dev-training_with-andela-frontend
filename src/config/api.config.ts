cf54fe9d42930d0af17b37434c60ecbec8866f53
Owen Kelvin <otienoowenkelvin@gmail.com>
2019-11-20 16:29:32 +0300

    - Dev link changed to https://capstone-project-teamwork.herokuapp.com/

--- src/config/api.config.ts
+++ src/config/api.config.ts
@@ -1,6 +1,6 @@
 let apiLink: string = 'http://localhost:3000/api/v1';
 if (process.env.NODE_ENV === "development") {
-    apiLink = "https://owen-capstone-project-teamwork.herokuapp.com/api/v1";
+    apiLink = "https://capstone-project-teamwork.herokuapp.com/api/v1";
 }
 
 export { apiLink }
